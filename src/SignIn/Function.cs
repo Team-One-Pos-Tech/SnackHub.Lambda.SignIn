25762534c43e7b77f9d447ea69aea2b395d1c62d
Kássio San <kciosantana@gmail.com>
2024-08-31 10:22:40 -0300

    Change project name

--- src/SignIn/Function.cs
+++ src/SignIn/Function.cs
@@ -11,7 +11,7 @@ using Amazon.Lambda.APIGatewayEvents;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace HelloWorld
+namespace SignIn
 {
 
     public class Function
