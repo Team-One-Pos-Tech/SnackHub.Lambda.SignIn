5c532c78dd608abaad80f7eb4db40fb3452358a0
Kássio San <kciosantana@gmail.com>
2024-09-20 19:49:17 -0300

    remove register

--- src/SignIn/Function.cs
+++ src/SignIn/Function.cs
@@ -12,59 +12,34 @@ using SignIn.Repositories;
 
 namespace SignIn
 {
-    public record SignUpBodyRequest(string Username, string Password, string Email);
+    public record SignInBodyRequest(string Username, string Password, string Email);
 
     public class Function
     {
         private readonly ISignInService _signInService;
 
-        private readonly ISignUpRepository _singUpRepository;
-
         public Function()
         {
             _signInService = new CognitoSignInService();
-            _singUpRepository = new CognitoSignUpRepository();
         }
 
         public Function(ISignInService signInService, ISignUpRepository singUpRepository)
         {
             _signInService = signInService;
-            _singUpRepository = singUpRepository;
         }
 
         public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent,
             ILambdaContext context)
         {
-            var requestBody = JsonSerializer.Deserialize<SignUpBodyRequest>(apigProxyEvent.Body);
+            var requestBody = JsonSerializer.Deserialize<SignInBodyRequest>(apigProxyEvent.Body);
 
             var signInResponse = await _signInService.Authenticate(
                 requestBody.Username,
                 requestBody.Password);
-
-            if (signInResponse.Success)
-            {
-                return CreateResponse(signInResponse);
-            }
-
-            var signUpRequest = new SignUpRequest(
-                requestBody.Username,
-                requestBody.Password,
-                requestBody.Email);
-
-            await _singUpRepository.Register(signUpRequest);
-
-            signInResponse = await _signInService.Authenticate(
-                requestBody.Username, 
-                requestBody.Password);
-
-            return CreateResponse(signInResponse);
-        }
-
-        private static APIGatewayProxyResponse CreateResponse(SingInResponse authResponse)
-        {
+            
             return new APIGatewayProxyResponse
             {
-                Body = JsonSerializer.Serialize(authResponse),
+                Body = JsonSerializer.Serialize(signInResponse),
                 StatusCode = 200,
                 Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
